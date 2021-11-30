dfd6e17dbc7bc88f941575e93956a354cba4b987
Margot Farkas <margotfrks@gmail.com>
2021-11-30 11:07:02 +0100

    deplacements a beger qui fonctionnent, tp en cours

---
+++ Assets/Scripts/XRRayInteractor_Custom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRRayInteractor_Custom : XRRayInteractor
+{
+
+    protected override void OnSelectEntering(XRBaseInteractable interactable)
+    {
+        base.OnSelectEntering(interactable);
+
+        if (!useForceGrab)
+        {
+            if (GetCurrentRaycastHit(out var raycastHit))
+            {
+                attachTransform.SetPositionAndRotation(
+                    interactable.transform.position +
+                        (gameObject.transform.position - raycastHit.point),
+                    interactable.transform.rotation);
+            }
+        }
+        else
+        {
+            attachTransform.SetPositionAndRotation(
+                interactable.transform.position,
+                interactable.transform.rotation);
+        }
+    }
+}
