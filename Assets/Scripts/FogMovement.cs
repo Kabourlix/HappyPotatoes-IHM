75ae7786be45715f12229cfbfdc0bb45746ee386
Maelys <maelys.chevrier@imt-atlantique.net>
2021-12-07 09:16:08 +0100

    ajout du brouillard et de son déplacement

---
+++ Assets/Scripts/FogMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Camera maincamera;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.SetPositionAndRotation(maincamera.transform.position, new Quaternion(0,0,0,0));
+    }
+}
