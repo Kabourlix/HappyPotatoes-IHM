1e1824d617fbb1c6563ecf967260c127479ff1db
Maelys <maelys.chevrier@imt-atlantique.net>
2021-12-07 17:02:52 +0100

    création frelons, update target, debut de comportement des frelons

---
+++ Assets/Scripts/Bees/AttackedBees.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackedBees : MonoBehaviour
+{
+
+    public Transform firsttarget;
+    // Animations of the Bee
+    private Animation animations;
+
+    // Life of the Bee
+    public float BeeHealth;
+    private bool isDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animations = gameObject.GetComponent<Animation>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (firsttarget.GetComponent<AttackedBees>().IsDead){
+         GetComponent<TranslationBees>().enabled=false;
+         GetComponent<TranslationBees>().enabled=false;
+        
+        }
+    }
+
+    public void ApplyDammage(float TheDammage)
+    {
+        if (!isDead)
+        {
+            BeeHealth = BeeHealth - TheDammage;
+            animations.Play("Damage");
+            if (BeeHealth <= 0)
+            {
+                Dead();
+            }
+        }
+    }
+
+    public void Dead()
+    {
+        gameObject.GetComponent<CapsuleCollider>().enabled = false;
+        isDead = true;
+        animations.Play("Death");
+        Destroy(transform.gameObject, 5);
+    }
+    public bool IsDead
+    {
+        get { return isDead; }
+        set { isDead = value; }
+
+    }
+}
---
+++ Assets/Scripts/Hornetscript.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hornetscript : MonoBehaviour
+{
+
+    // Distance between the bee or the player  and the hornet
+    private float TargetDistance;
+    private float Playerdistance;
+
+    // List of potential
+    public Transform currenttarget;
+    public Transform[] targets;
+    public Transform Player;
+
+    //Distance of pursuit
+    public float chaseRange = 10;
+
+    // Range of the attacks
+    public float attackRange = 2.2f;
+
+    // Cooldown of attacks
+    public float attackRepeatTime = 1;
+    private float attackTime;
+
+    // Amount of the inflicted dammage
+    public float TheDammage;
+
+    // Agent de navigation
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    // Animations  of the enemy
+    private Animation animations;
+
+    // Life of the enemy
+    public float enemyHealth;
+    private bool isDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>(); 
+        animations = gameObject.GetComponent<Animation>();
+        attackTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updatetarget();
+
+        // On calcule la distance entre le joueur et l'enemy, en fonction de cette distance on effectue diverses actions
+        TargetDistance = Vector3.Distance(currenttarget.position, transform.position);
+
+        // Quand l'ennemi est proche mais pas assez pour attaquer
+        if (TargetDistance < chaseRange && TargetDistance > attackRange)
+        {
+            chase();
+        }
+
+        // Quand l'enemy est assez proche pour attaquer
+        if (TargetDistance < attackRange)
+        {
+            attack();
+        }
+    }
+
+    // pursuit
+    void chase()
+    {
+        animations.Play("Move");
+        agent.destination = currenttarget.position;
+    }
+
+    void attack()
+    {
+        // empeche l'enemy de traverser le joueur
+        agent.destination = transform.position;
+
+        //Si pas de cooldown
+        if (Time.time > attackTime)
+        {
+            animations.Play("Attack");
+            currenttarget.GetComponent<AttackedBees>().ApplyDammage(TheDammage);
+            //Debug.Log("L'ennemi a envoyÃ© " + TheDammage + " points de dÃ©gÃ¢ts");
+            attackTime = Time.time + attackRepeatTime;
+        }
+    }
+
+    public void ApplyDammage(float TheDammage)
+    {
+        if (!isDead)
+        {
+            enemyHealth = enemyHealth - TheDammage;
+            animations.Play("Damage");
+            if (enemyHealth <= 0)
+            {
+                Dead();
+            }
+        }
+    }
+    public void Dead()
+    {
+        gameObject.GetComponent<CapsuleCollider>().enabled = false;
+        isDead = true;
+        animations.Play("Death");
+        Destroy(transform.gameObject, 5);
+    }
+
+    public void updatetarget()
+    {
+        Playerdistance = Vector3.Distance(Player.position, transform.position);
+        if (Playerdistance<=attackRange)
+        {
+            currenttarget = Player;
+        }
+        else if (targets.Length>0 )
+        {
+            currenttarget = targets[0];
+        }
+        else
+        {
+            currenttarget = Player;
+        }
+    }
+}
