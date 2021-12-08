a339c6093ae70b3bac802dd12afb7f724b1e9b17
Maelle GABENS <gabens.maelle@gmail.com>
2021-12-08 16:00:48 +0100

    ui work on menu scene

--- Assets/Scripts/Menu/SettingButton.cs
+++ Assets/Scripts/Menu/SettingButton.cs
@@ -1,28 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class UIController : MonoBehaviour
+public class Go2menu : MonoBehaviour
 {
-
     public GameObject Menu;
     public GameObject Settings;
-    public Button SettingButton;
     public bool disable;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         disable = true;
         Menu.SetActive(true);
         Settings.SetActive(false);
-        Button btn = SettingButton.GetComponent<Button>();
-        btn.onClick.AddListener(changeCanvasProperty);
     }
-
-    void changeCanvasProperty()
+    public void EnableMenu()
     {
         disable = !disable;
         Menu.gameObject.SetActive(disable);
@@ -30,9 +22,4 @@ public class UIController : MonoBehaviour
     }
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
