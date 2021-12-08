4131840116366f31b89c22182ba45f2bf6fcabaa
Margot Farkas <margotfrks@gmail.com>
2021-12-08 09:58:06 +0100

    bechage elementaire qui marche

---
+++ Assets/Scripts/Digging.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Digging : MonoBehaviour
+{
+
+    public GameObject mud;
+    private int maxHits;
+    private int nbHits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mud.SetActive(false);
+        maxHits = 10;
+        nbHits = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Shovel")
+        {
+            if (nbHits >= maxHits)
+            {
+                mud.SetActive(true);
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                nbHits++;
+            }
+        }
+    }
+}
