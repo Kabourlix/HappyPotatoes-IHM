f909564695b43fc014337ca963ba1e438c0b7ba1
hdamaia <hugo.da-maia@imt-atlantique.net>
2021-12-08 14:59:34 +0100

    Enhanced the resolution of the UI

--- Assets/Scripts/Shop/ShopUIInteraction.cs
+++ Assets/Scripts/Shop/ShopUIInteraction.cs
@@ -6,7 +6,14 @@ namespace Shop
 {
     public class ShopUIInteraction : MonoBehaviour
     {
-
+        //UI Objects
+        
+        
+        // end
+        
+        
+        
+        
         private List<GameObject> children;
         private int modeActive;
         private void Start()
@@ -28,20 +35,32 @@ namespace Shop
             modeActive = 0;
             children[modeActive].SetActive(true);
         }
+        
+        private void PerformSwitchMode(int mode)
+        {
+            foreach (GameObject child in children)
+            {
+                child.SetActive(false);
+            }
 
+            modeActive = mode;
+            children[modeActive].SetActive(true);
+        }
+        
+        // Switching interface
         public void GoToBuyPage() // mode = 1
         {
-            
+            PerformSwitchMode(1);
         }
 
         public void GoToWelcomePage() //Mode = 0
         {
-            
+            PerformSwitchMode(0);
         }
 
         public void GoToSellPage() // mode = 2
         {
-            
+            PerformSwitchMode(2);
         }
     }
 }
