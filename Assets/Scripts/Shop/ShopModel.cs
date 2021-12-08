da81e3614fa4ace17962ba9cc10580a23357141e
hdamaia <hugo.da-maia@imt-atlantique.net>
2021-12-08 16:25:23 +0100

    Create a struct to gather the item info

--- Assets/Scripts/Shop/ShopModel.cs
+++ Assets/Scripts/Shop/ShopModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Shop
@@ -6,7 +7,7 @@ namespace Shop
     public class ShopModel : MonoBehaviour
     {
         // Data in the shop, we store it here since it's tiny data
-        
+        private List<Item> items; 
         
         public int ModeActive { get; set; }
 
@@ -22,6 +23,33 @@ namespace Shop
             }
 
             Instance = this;
+        }
+        
+        struct Item
+        {
+            private int id;
+            private string Name { get; }
+            private bool isSellable; // Initially on true 
+            public string Path { get; }
+            public string Info { get; }
+            public int Price { get; }
+
+            public Item(string name,string imgPath, string info, int price)
+            {
+                id = Instance.items.Count+1;
+                this.Name = name;
+                isSellable = true;
+                Path = imgPath; //! This might causes issue in the future (if the image is not loaded for example)
+                this.Info = info; //Initialisation
+                this.Price = price;
+            }
+        
+            public void SetSellable(bool b)
+            {
+                isSellable = b;
+            }
+            
+        
         }
     }
 }
--- Assets/Scripts/Shop/ShopUIInteraction.cs
+++ Assets/Scripts/Shop/ShopUIInteraction.cs
@@ -80,4 +80,5 @@ namespace Shop
             currencyText.text = value.ToString();
         }
     }
+    
 }
