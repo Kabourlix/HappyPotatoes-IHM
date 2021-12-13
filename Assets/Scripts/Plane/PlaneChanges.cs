edd0e48469a683bcde9b0bc08fce71bc5528b8c9
Maelle GABENS <gabens.maelle@gmail.com>
2021-12-13 19:13:52 +0100

    add drought

---
+++ Assets/Scripts/Plane/PlaneChanges.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using events;
+
+public class PlaneChanges : MonoBehaviour
+{
+
+    [SerializeField] private Material earth;
+    [SerializeField] private Material green;
+    [SerializeField] private Renderer rend;
+    [SerializeField] private GameObject dryPlants;
+
+    void Awake()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        launch();
+        DroughtPlane();
+        DroughtEvent.current.onDroughtEvent += DroughtPlane;
+    }
+
+    // Update is called once per frame
+    public void DroughtPlane()
+    {
+        rend.material = earth;
+        dryPlants.SetActive(false);
+    }
+
+    public void launch()
+    {
+        rend.material = green;
+    }
+}
--- Assets/Scripts/events/DroughtEvent.cs
+++ Assets/Scripts/events/DroughtEvent.cs
@@ -1,21 +1,29 @@
 using UnityEngine;
+using System;
 
 namespace events
 {
     public class DroughtEvent : GameEvent
     {
+        public static DroughtEvent current;
+        private void Awake()
+        {
+            current = this;
+        }
         public DroughtEvent(string _name, GameObject _relatedObject) : base(_name, _relatedObject)
         {
         }
 
+        public event Action onDroughtEvent;
         public override void PlayEventSequence()
         {
-            throw new System.NotImplementedException();
+            onDroughtEvent();
         }
 
+        public event Action onMusicSpeed;
         public override void PlaySoundSequence()
         {
-            throw new System.NotImplementedException();
+            onMusicSpeed();
         }
     }
 }
