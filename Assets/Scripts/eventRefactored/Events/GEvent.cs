1f49e10c83a856683730d7b340ec8b2792ad6873
hdamaia <hugo.da-maia@imt-atlantique.net>
2021-12-14 02:23:23 +0100

    Started reimplementing the event system.

---
+++ Assets/Scripts/eventRefactored/Events/GEvent.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+namespace eventRefactored.Events
+{
+    /// <summary>
+    /// This class contains the information needed for each event.
+    /// Each event will be its own class inherited from GEvent.
+    /// </summary>
+    public abstract class GEvent : MonoBehaviour
+    {
+        // Attributes
+        public int ID { get; private set;}
+
+        private bool Status
+        {
+            get => Status;
+            set
+            {
+                if (value) return;
+                EndSequence();
+                manager.DeleteGEventFromStack(ID);
+            }
+        } //true if on going, false if to be ended.
+        
+        private EventsManager manager;
+
+
+        private void Awake()
+        {
+            manager = EventsManager.Instance;
+        }
+        
+        /// <summary>
+        /// This function is the core of the GEvent.
+        /// It is here that must be placed all the init action of an event :
+        ///     - Instantiates GameObjects
+        ///     - Change Fox Target
+        ///     - Activate mode (for exemple Hornet following bees mode)
+        /// </summary>
+        public abstract void LaunchSequence();
+        
+        /// <summary>
+        /// This function will end the GEvent.
+        /// It is called when all condition are united to end the event (This will be changed in the Update loop).
+        /// Therefore, any destruction, mode changes et fox behavior must be readjusted in consequences here.
+        /// </summary>
+        protected abstract void EndSequence();
+    }
+}
---
+++ Assets/Scripts/eventRefactored/EventsManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using eventRefactored.Events;
+using UnityEngine;
+
+namespace eventRefactored
+{
+    public class EventsManager : MonoBehaviour
+    {
+        private List<GEvent> eventStack;
+        
+        private GEvent NewEvent
+        {
+            get => NewEvent;
+            set => eventStack.Add(value);
+        }
+
+        private int eventRate; // This is the GEvent apparition frequency, in event per minutes.
+
+        private SettingsManagers settings; // This will be usefull to get world constant and parameters.
+
+        public static EventsManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+            }
+
+            Instance = this; 
+            
+            settings = SettingsManagers.Instance;
+        }
+
+        /// <summary>
+        /// In the loop Update, we want to randomly generates a event triggers that will change the amountEvent variables,
+        /// which will trigger the OnEventTrigger event (C#).
+        /// </summary>
+        private void Update()
+        {
+            // We randomly take an event or not.
+            
+            //If we add one, we launch sequence
+            
+        
+        }
+
+        public void DeleteGEventFromStack(int id)
+        {
+            foreach (var gEvent in eventStack.Where(gEvent => gEvent.ID == id))
+            {
+                eventStack.Remove(gEvent);
+                break;
+            }
+        }
+        
+    }
+}
