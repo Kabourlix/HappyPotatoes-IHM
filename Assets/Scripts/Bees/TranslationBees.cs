af13fe206de46bf171d3634a17ce3e2d169b8c50
Maelys <maelys.chevrier@imt-atlantique.net>
2021-12-14 02:18:40 +0100

    parite 1

--- Assets/Scripts/Bees/TranslationBees.cs
+++ Assets/Scripts/Bees/TranslationBees.cs
@@ -2,11 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
  
-
+//
 public class TranslationBees : MonoBehaviour
 {
     public float oscillationspeed = 1.0f;
-
 
     // Start is called before the first frame update
     void Start()
